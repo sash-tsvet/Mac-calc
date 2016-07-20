811985e250dc83851eee76ebeac4e3146e3e5291
Alexandra <alexandra@sts-imac-3.local>
2016-07-20 12:47:59 +0300

    Initial Commit

---
+++ calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calculator
+//
+//  Created by Alexandra on 20/07/16.
+//  Copyright © 2016 Alexandra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  calculator
+//
+//  Created by Alexandra on 20/07/16.
+//  Copyright © 2016 Alexandra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
